489371de950715eda05de17e9153317b99a3bed7
Atlas Kerr <atlaskerr@gmail.com>
2017-04-26 12:23:02 -0500

    added public and private ips to module output

--- output.tf
+++ output.tf
@@ -1,3 +1,11 @@
 output "id" {
 	value = "{ aws_instance.container_linux.id }"
 }
+
+output "public_ip" {
+	value = "{ aws_instance.container_linux.public_ip }"
+}
+
+output "private_ip" {
+	value = "{ aws_instance.container_linux.private_ip }"
+}
